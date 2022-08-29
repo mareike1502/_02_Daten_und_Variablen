3d775f0005a52f2f069caac4c143ae178893056f
mareike1502 <111485353+mareike1502@users.noreply.github.com>
2022-08-29 18:08:37 +0200

    Add files via upload

---
+++ 02_Daten_und_Variablen.java
@@ -0,0 +1,60 @@
+public static void main(String[]args){
+
+       // 1. Datentypen
+        int alter = 17;
+        String zahl ("17");
+        double sparschwein = 193.80;
+        char anzahl = 7;
+        double pi = 3.14159
+        String Warnung = "Achtung! Ein Sturm kommt.";
+        String  Frage = "Wie heißt du?";
+        char anfangsBuchstabe = 'B';
+        double summe = (double)anzahl * sparschwein
+        char endBuchstabe = "L";
+        String abc = 'ABC';
+
+        // 2. String-Konkatenation (1)
+        String word1 = "Brautkleid";
+        String word2 = "bleibt";
+        String word3 = "Brautkleid";
+        String word4 = "und";
+        String word5 = "Blaukraut";
+        String word6 = "bleibt";
+        String word7 = "Blaukraut";
+        System.out.println("word1 + word2 + word3 + word4 + word5 + word6 + word7");
+
+        // 3. String-Konkatenation (2)
+        int zahl1 = 9;
+        int zahl2 = 6;
+        System.out.println(zahl1 + "+" + zahl2 + "=" + zahl1 + zahl2);
+        // Wiedergegeben wird " 9+6=15", die Zahlen kommen durch di int Anweisungen, das eine plus ist in " gestellt
+        // damit es nicht benutzt wird um die Anweisungen zu verbinden sondern angezeigt wird, genau so wie das =.
+        // Am Ende werden die Zahlen von IntelliJ addiert.
+
+        // 4. Arithmetische Operatoren
+        int zahl1 = 21;
+        int zahl2 = 3;
+        int zahl3 = 17;
+        int zahl4 = 4;
+        System.out.println(zahl1 + "*" + zahl2 + "+" + zahl3 + "*" + zahl4 + "=" + zahl1 * Zahl2 + zahl3 * zahl4);
+
+
+        //5. Arithmetische Operatoren (2)
+        int jahre = 20;
+        int monate = 3;
+        int tage = 7;
+        int stunden = 18;
+        int Minuten = 5;
+        int monate/jahr = 12;
+        int tage/monat = 30;
+        int tage/jahr = 365;
+        int tageSchaltjahr = 5;
+        System.out.println(jahre + "*" + monate/jahr + "+" monate + "=" + jahre * monate/jahr + monate);
+        System.out.println(monate + "*" + tage/monat + "+" tage/jahr + "*" + jahre + "+" + tageSchaltjahr + "+" + )
+
+
+
+
+
+
+}
